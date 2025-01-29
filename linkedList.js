3be8d5c0bd3b40a81050cd339c4a1fc18a3a5532
mukeshkumar098 <mukeshkemar567@gmail.com>
2025-01-29 15:35:58 +0530

    check

--- linkedList.js
+++ linkedList.js
@@ -1,30 +1,30 @@
-class Node {
-    constructor(data) {
-      this.data = data;
-      this.next = null;
-    }
-  }
+// class Node {
+//     constructor(data) {
+//       this.data = data;
+//       this.next = null;
+//     }
+//   }
 
-  let a = new Node(1);
-  let b = new Node(2);
-  let c = new Node(3);
+//   let a = new Node(1);
+//   let b = new Node(2);
+//   let c = new Node(3);
   
-  a.next = b;
-  b.next = c;
+//   a.next = b;
+//   b.next = c;
   
-  let head = a;
+//   let head = a;
   
-  let d = new Node(4);
-  d.next = head;
-  head = d;
+//   let d = new Node(4);
+//   d.next = head;
+//   head = d;
   
-  let current = head;
-  let result = [];
-  while (current != null) {
-    result.push(current.data);
-    current = current.next;
-  }
-  console.log("Linked List after inserting at the beginning:", result.join(" -> "));
+//   let current = head;
+//   let result = [];
+//   while (current != null) {
+//     result.push(current.data);
+//     current = current.next;
+//   }
+//   console.log("Linked List after inserting at the beginning:", result.join(" -> "));
 
 
 // Q:-second
@@ -62,7 +62,7 @@ class Node {
 //   console.log("Linked List after inserting at the end:", result.join(" -> "));
 
 
-// 3 ----->
+// Q;-3 ----->
 // class Node {
 //     constructor(data) {
 //       this.data = data;
@@ -104,3 +104,44 @@ class Node {
 //     current = current.next;
 //   }
 //   console.log(Linked List after inserting at position ${position}:, result.join(" -> "));
+
+
+
+// delete node 
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+let a = new ListNode(1);
+let b = new ListNode(2);
+let c= new ListNode(3);
+let  d= new ListNode(4);
+  a.next=b;
+  b.next=c;
+  c.next=d
+head=a
+
+function deleteNode(head, value) {
+  if (!head) return null;
+
+  if (head.data === value) return head.next; 
+
+  let current = head;
+  while (current.next && current.next.data !== value) {
+    current = current.next;
+  }
+
+  if (current.next) {
+    current.next = current.next.next; 
+  }
+
+  return head;
+}
+
+
+
+console.log(deleteNode(head, 3)); 
+
